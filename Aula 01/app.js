f3e3c1fb1d98b7b19dc8f5129245640e3df344df
Rodrigo Talisson <rodrilek.azevedo@gmail.com>
2026-01-14 21:32:06 -0300

    po

--- Aula 01/app.js
+++ Aula 01/app.js
@@ -1,8 +1,34 @@
-import express from "express";
+import express from "express"; // importa o framework express
 
-const app = express();
-const port = 3000;
+const app = express(); // cria a aplicação
+const port = 3000; // porta da API
 
-app get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+    res.send("Hello World!!!");
+});
+
+app.get("/teste", (req, res) => {
+    res.send("Hello World meu fi!!!");
+});
+
+app.get("/usuario", (req, res) => {
+    res.json({
+        nome: "Rodrigo",
+        idade: 15,
+        cidade: "Natal"
+    });
+});
+app.get("/status", (req, res) => {
+    res.status(200),send("Status ok");
+});
+app.get("/usuario/:id", (req, res)=>{
+    const {id}=req.params;
+    res.status(200), json({
+        id:id,
+        nome: "User exemplo"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Aplicação rodando em http://localhost:${port}`);
 });
